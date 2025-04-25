89670b1b7979df778ef926806aaa1b92ac9700d8
ceteper <75122254@qq.com>
2025-04-25 17:21:59 +0800

    增加注释

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@
  * @Author: ceteper 75122254@qq.com
  * @Date: 2025-04-24 14:29:00
  * @LastEditors: ceteper 75122254@qq.com
- * @LastEditTime: 2025-04-25 14:35:01
+ * @LastEditTime: 2025-04-25 17:18:48
  * @FilePath: \mmcl\src\main.tsx
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @Description: Main Router
  */
 import React from "react";
 import ReactDOM from "react-dom/client";
