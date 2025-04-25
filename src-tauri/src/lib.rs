1f39a7dd30e5cf8af3cf88164a0555f95fa83be6
ALBamKin <albamkin@ALBamKindeMacBook-Pro.local>
2025-04-25 14:44:16 +0800

    test url versoin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,20 @@
+use serde_json::Value;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[tauri::command]
+async fn get_mc_version() -> Result<String, String> {
+    let response = reqwest::get("https://launchermeta.mojang.com/mc/game/version_manifest.json")
+        .await
+        .map_err(|e| e.to_string())?;
+
+    let json: Value = response.json::<Value>().await.map_err(|e| e.to_string())?;
+
+    Ok(json.to_string())
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
