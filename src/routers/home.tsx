fc510793c88cae35bb8e297445a2fdd7e20dd4d0
ceteper <75122254@qq.com>
2025-04-27 11:10:44 +0800

    实现首页基础样

--- src/routers/home.tsx
+++ src/routers/home.tsx
@@ -1,9 +1,41 @@
+import { motion } from "motion/react";
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { Play, Rocket, ChevronRight } from "lucide-react";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <ScrollArea className="flex-1 h-full">
+        <motion.div
+          initial={{ opacity: 0, y: 0 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: 0 }}
+          transition={{ duration: 0.5 }}
+          className="w-full h-full p-6 flex flex-col flex-1">
+
+          <div className="flex flex-col mb-4">
+            <h1 className="text-2xl font-bold">首 页</h1>
+            <p className="text-sm text-gray-500">欢迎来到MMCL</p>
+          </div>
+          <div className="grid grid-cols-2 gap-4 w-full">
+
+          </div>
+          <Button className="group overflow-hidden border-none shadow-lg px-6 h-16 absolute bottom-6 right-6 z-10">
+            <div className="absolute inset-0 w-full h-full opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
+            <div className="relative z-10 flex items-center justify-center gap-4">
+              <Rocket className="w-5 h-5" />
+              <div className="flex flex-col items-start">
+                <span className="text-lg font-bold leading-none mb-0.5">启动游戏</span>
+                <span className="text-xs text-muted-foreground">Minecraft 1.12.1</span>
+              </div>
+              <ChevronRight className="w-5 h-5 opacity-70 group-hover:translate-x-1 transition-transform" />
+            </div>
+          </Button>
+        </motion.div>
+      </ScrollArea>
+    </>
   )
 }
